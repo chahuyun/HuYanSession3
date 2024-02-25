c235ca3e4ca5d4fa6c2cd9514b8415c13eb995b3
Moyuyanli <572490972@qq.com>
2024-02-25 14:31:34 +0800

    refactor:补上缓存缺少的api

--- src/main/java/cn/chahuyun/session/data/cache/Cache.java
+++ src/main/java/cn/chahuyun/session/data/cache/Cache.java
@@ -1,9 +1,12 @@
 package cn.chahuyun.session.data.cache;
 
+import cn.chahuyun.session.data.Scope;
 import cn.chahuyun.session.data.entity.ManySession;
 import cn.chahuyun.session.data.entity.Permission;
 import cn.chahuyun.session.data.entity.SingleSession;
 import cn.chahuyun.session.data.entity.TimingSession;
+
+import java.util.List;
 
 /**
  * 缓存api
@@ -74,6 +77,39 @@ public interface Cache {
     Permission getPermissions(Integer id);
 
     /**
+     * 获取单一消息<br>
+     * 需要按照一下顺序排序后的结果:<br>
+     * <ui></ui>
+     * @param scope 作用域
+     * @return 单一消息集合
+     */
+    List<SingleSession> getSingSession(Scope scope);
+
+    /**
+     * 获取多词条消息
+     *
+     * @param scope 作用域
+     * @return 多词条消息集合
+     */
+    List<ManySession> getManySession(Scope scope);
+
+    /**
+     * 获取定时消息
+     *
+     * @param scope 作用域
+     * @return 定时消息集合
+     */
+    List<TimingSession> getTimingSession(Scope scope);
+
+    /**
+     * 获取权限信息
+     *
+     * @param scope 作用域
+     * @return 权限信息集合
+     */
+    List<Permission> getPermissions(Scope scope);
+
+    /**
      * 删除单一消息
      *
      * @param id 消息id
@@ -100,6 +136,16 @@ public interface Cache {
      * @param id 权限信息
      */
     void removePermissions(Integer id);
+
+    /**
+     * 获取所有作用域
+     *
+     * @return List<Scope> 作用域集合
+     */
+    List<Scope> getAllScope();
+
+
+
 
 
 }
