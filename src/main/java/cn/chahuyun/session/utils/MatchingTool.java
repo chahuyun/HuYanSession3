ab951d549252ff42052e2f53e20525504ec27156
Moyuyanli <572490972@qq.com>
2024-02-25 14:30:32 +0800

    feat:匹配工具

---
+++ src/main/java/cn/chahuyun/session/utils/MatchingTool.java
@@ -0,0 +1,35 @@
+package cn.chahuyun.session.utils;
+
+import cn.chahuyun.session.data.Scope;
+import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.contact.User;
+
+/**
+ * 匹配工具
+ *
+ * <p>构建时间: 2024/2/25 13:55</p>
+ *
+ * @author Moyuyanli
+ */
+public class MatchingTool {
+
+    private MatchingTool() {
+    }
+
+
+    /**
+     * 匹配作用域
+     *
+     * @param scope   作用域
+     * @param subject 消息事件主体
+     * @param sender  发送着
+     * @return true 匹配成功
+     */
+    public static boolean matchScope(Scope scope, Contact subject, User sender) {
+
+
+        return false;
+    }
+
+
+}
