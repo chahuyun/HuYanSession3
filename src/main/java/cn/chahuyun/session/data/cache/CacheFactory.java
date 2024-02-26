2ef95b4c7fd2086c56e84d8d3c270f1f2783d0e3
Moyuyanli <RoxyGreerad@163.com>
2024-02-26 09:35:59 +0800

    feat:初始化缓存数据

--- src/main/java/cn/chahuyun/session/data/cache/CacheFactory.java
+++ src/main/java/cn/chahuyun/session/data/cache/CacheFactory.java
@@ -1,6 +1,14 @@
 package cn.chahuyun.session.data.cache;
 
+import cn.chahuyun.session.data.entity.ManySession;
+import cn.chahuyun.session.data.entity.Permission;
+import cn.chahuyun.session.data.entity.SingleSession;
+import cn.chahuyun.session.data.entity.TimingSession;
+import cn.chahuyun.session.data.factory.AbstractDataService;
+import cn.chahuyun.session.data.factory.DataFactory;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 /**
  * 缓存工厂
@@ -31,6 +39,18 @@ public class CacheFactory {
             throw new RuntimeException("缓存工厂初始化失败:缓存服务为空!");
         }
         instance = new CacheFactory(cache);
+        AbstractDataService dataService = DataFactory.getInstance().getDataService();
+
+        List<SingleSession> singleSessions = dataService.selectListEntity(SingleSession.class, "from SingleSession");
+        List<ManySession> manySessions = dataService.selectListEntity(ManySession.class, "from ManySession");
+        List<TimingSession> timingSessions = dataService.selectListEntity(TimingSession.class, "from TimingSession");
+        List<Permission> permissions = dataService.selectListEntity(Permission.class, "from Permission");
+
+        singleSessions.forEach(it -> instance.cacheService.putSession(it));
+        manySessions.forEach(it -> instance.cacheService.putSession(it));
+        timingSessions.forEach(it -> instance.cacheService.putSession(it));
+        permissions.forEach(it -> instance.cacheService.putPermission(it));
+
         log.debug("缓存服务初始化完成!");
     }
 
