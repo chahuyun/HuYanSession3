e8cd28cd130c07d9b9a54b9d4c4d6cb10c26bde3
Moyuyanli <RoxyGreerad@163.com>
2024-02-26 19:58:07 +0800

    fix:修复异常处理的继承问题

---
+++ src/main/kotlin/cn/chahuyun/session/exception/ExceptionHandle.kt
@@ -0,0 +1,21 @@
+package cn.chahuyun.session.exception
+
+import cn.chahuyun.session.HuYanSession
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * 全局异常处理
+ *
+ * @author Moyuyanli
+ * @date 2024/1/15 11:20
+ */
+class ExceptionHandle : CoroutineExceptionHandler {
+    
+    override val key: CoroutineContext.Key<*>
+        get() = CoroutineExceptionHandler.Key
+
+    override fun handleException(coroutineContext: CoroutineContext, throwable: Throwable) {
+        HuYanSession.INSTANCE.logger.error(throwable)
+    }
+}
