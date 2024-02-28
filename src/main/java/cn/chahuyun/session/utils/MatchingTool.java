3069c25032cfbad004a2afe54910050050787384
Moyuyanli <RoxyGreerad@163.com>
2024-02-28 11:26:28 +0800

    feat:完成作用域匹配和触发词匹配两个工具方法

--- src/main/java/cn/chahuyun/session/utils/MatchingTool.java
+++ src/main/java/cn/chahuyun/session/utils/MatchingTool.java
@@ -7,6 +7,8 @@ import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.message.data.MessageChain;
 
+import java.util.regex.Pattern;
+
 /**
  * 匹配工具
  *
@@ -29,25 +31,75 @@ public class MatchingTool {
      * @return true 匹配成功
      */
     public static boolean matchScope(Scope scope, Contact subject, User sender) {
-
-
+        switch (scope.getType()) {
+            case USERS:
+                return scope.getUsers().contains(sender.getId());
+            case LIST:
+                return scope.getGroups().contains(subject.getId());
+            case GROUP_MEMBERS:
+                return scope.getGroup() == subject.getId() && scope.getMembers().contains(sender.getId());
+            case GROUP_MEMBER:
+                return scope.getGroup() == subject.getId() && scope.getMember() == sender.getId();
+            case GROUP:
+                return scope.getGroup() == subject.getId();
+            case GLOBAL_USER:
+                return scope.getUser() == sender.getId();
+            case GLOBAL:
+                return true;
+        }
         return false;
     }
 
     /**
      * 匹配单一消息的触发词
+     *
      * @param singleSession 单一消息
-     * @param messages 消息事件
+     * @param messages      消息事件
      * @return true 成功
      */
     public static boolean matchTrigger(SingleSession singleSession, MessageChain messages) {
-        return false;
+        String matchString;
+
+        switch (singleSession.getConversionType()) {
+            case STRING:
+                matchString = messages.toString();
+                break;
+            case CONTENT:
+                matchString = messages.contentToString();
+                break;
+            case JSON:
+                matchString = MessageChain.serializeToJsonString(messages);
+                break;
+            case MIRAI_CODE:
+                matchString = messages.serializeToMiraiCode();
+                break;
+            default:
+                throw new RuntimeException("消息转换类型错误!");
+        }
+
+
+        String trigger = singleSession.getTrigger();
+        switch (singleSession.getTriggerType()) {
+            case PRECISION:
+                return matchString.equals(trigger);
+            case FUZZY:
+                return matchString.contains(trigger);
+            case HEAD:
+                return matchString.startsWith(trigger);
+            case TAIL:
+                return matchString.endsWith(trigger);
+            case REGULAR:
+                return Pattern.matches(trigger, matchString);
+            default:
+                return false;
+        }
     }
 
     /**
      * 匹配多词条消息的触发词
+     *
      * @param manySession 多词条消息
-     * @param messages 消息事件
+     * @param messages    消息事件
      * @return true 成功
      */
     public static boolean matchTrigger(ManySession manySession, MessageChain messages) {
