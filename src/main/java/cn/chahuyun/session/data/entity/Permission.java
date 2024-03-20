00c51fd8554fba0f5b4e21d01193aadbf5e0c588
Moyuyanli <RoxyGreerad@163.com>
2024-03-20 17:53:42 +0800

    refactor:修正权限设计

--- src/main/java/cn/chahuyun/session/data/entity/Permission.java
+++ src/main/java/cn/chahuyun/session/data/entity/Permission.java
@@ -1,7 +1,6 @@
 package cn.chahuyun.session.data.entity;
 
 import cn.chahuyun.session.data.BaseEntity;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -14,79 +13,16 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "permission")
 public class Permission extends BaseEntity {
-
     /**
-     * 管理权限
+     * 权限字符
      */
-    @Column(name = "`admin`")
-    private boolean admin;
-    /**
-     * 会话权限
-     */
-    private boolean session;
-    /**
-     * 多词条
-     */
-    private boolean dct;
-    /**
-     * 定时
-     */
-    private boolean ds;
-    /**
-     * 会话
-     */
-    private boolean hh;
-    /**
-     * 群组
-     */
-    private boolean list;
+    private String permCode;
 
-
-    public boolean isAdmin() {
-        return admin;
+    public String getPermCode() {
+        return permCode;
     }
 
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
-
-    public boolean isSession() {
-        return session;
-    }
-
-    public void setSession(boolean session) {
-        this.session = session;
-    }
-
-    public boolean isDct() {
-        return dct;
-    }
-
-    public void setDct(boolean dct) {
-        this.dct = dct;
-    }
-
-    public boolean isDs() {
-        return ds;
-    }
-
-    public void setDs(boolean ds) {
-        this.ds = ds;
-    }
-
-    public boolean isHh() {
-        return hh;
-    }
-
-    public void setHh(boolean hh) {
-        this.hh = hh;
-    }
-
-    public boolean isList() {
-        return list;
-    }
-
-    public void setList(boolean list) {
-        this.list = list;
+    public void setPermCode(String permCode) {
+        this.permCode = permCode;
     }
 }
