7f726dc7c185173c0a94294537314dde2364a06f
Moyuyanli <572490972@qq.com>
2025-07-26 19:34:41 +0800

    build: 更新项目依赖和仓库- 版本号从 0.0.9 升级到 0.0.10- 注释掉 mavenCentral仓库，使用 jsdu nexus 仓库 - 移除 mirai-hibernate-plugin 依赖 - 添加 hibernate-plus 依赖 - 更新 HuYanAuthorize 依赖版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 group = "cn.chahuyun"
-version = "0.0.9"
+version = "0.0.10"
 
 repositories {
-    mavenCentral()
-    maven("https://maven.aliyun.com/repository/public")
+//    mavenCentral()
+    maven("https://nexus.jsdu.cn/repository/maven-public/")
 }
 
 dependencies {
@@ -19,16 +19,18 @@ dependencies {
     //sqlite的方言支持
     api("org.hibernate.orm:hibernate-community-dialects:6.3.0.Final")
 
-    compileOnly ("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.7.1")
-    compileOnly("cn.chahuyun:HuYanAuthorize:1.0.7")
+//    compileOnly ("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.7.1")
+    implementation("cn.chahuyun:hibernate-plus:1.0.16")
+
+    compileOnly("cn.chahuyun:HuYanAuthorize:1.2.0")
 
     implementation("cn.chahuyun.api:huyan-public-api:0.0.1")
 
     implementation("org.projectlombok:lombok:1.18.26")
     annotationProcessor("org.projectlombok:lombok:1.18.26")
 
-    implementation ("cn.hutool:hutool-all:5.8.24")
-    testImplementation ("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.7.1")
+    implementation("cn.hutool:hutool-all:5.8.24")
+    testImplementation("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.7.1")
 
 
 }
